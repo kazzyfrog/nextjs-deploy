bfda0452b862d0b62dbe8b028d3924cbe787eaf9
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-04-17 04:59:36 +0900

    add dynamic og-image

---
+++ src/app/[slug]/opengraph-image.tsx
@@ -0,0 +1,102 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+// export const runtime = "edge";
+
+// Image metadata
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image({ params }: { params: { slug: string } }) {
+  // Font
+  //   const interSemiBold = fetch(
+  //     new URL("./Inter-SemiBold.ttf", import.meta.url)
+  //   ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      //   <div
+      //     style={{
+      //       fontSize: 128,
+      //       background: "white",
+      //       width: "100%",
+      //       height: "100%",
+      //       display: "flex",
+      //       alignItems: "center",
+      //       justifyContent: "center",
+      //     }}
+      //   >
+      //     About Acme
+      //   </div>
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          letterSpacing: "-.02em",
+          fontWeight: 700,
+          background: "white",
+        }}
+      >
+        <div
+          style={{
+            left: 42,
+            top: 42,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <span
+            style={{
+              width: 24,
+              height: 24,
+              background: "black",
+            }}
+          />
+          <span
+            style={{
+              marginLeft: 8,
+              fontSize: 20,
+            }}
+          >
+            Hi there 👋 Im {params.slug}
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            padding: "20px 50px",
+            margin: "0 42px",
+            fontSize: 40,
+            width: "auto",
+            maxWidth: 550,
+            textAlign: "center",
+            backgroundColor: "black",
+            color: "white",
+            lineHeight: 1.4,
+          }}
+        >
+          Making the Web. Faster.
+        </div>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
