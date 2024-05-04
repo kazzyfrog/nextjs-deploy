f92f3c0b20ce4a75dfcceb128288bb79acedc6a4
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-05-04 20:06:59 +0900

    demo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,71 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    keyframes: {
+      horizontal: {
+        from: {
+          transform: "translateX(0)",
+        },
+        to: {
+          transform: "translateX(100vw)",
+        },
+      },
+
+      vertical: {
+        from: {
+          transform: "translateY(0)",
+        },
+        to: {
+          transform: "translateY(100vh)",
+        },
+      },
+
+      spin: {
+        from: {
+          transform: "rotate(0deg)",
+        },
+        to: {
+          transform: "rotate(360deg)",
+        },
+      },
+
+      ping: {
+        "0%": {
+          transform: "scale(1)",
+          opacity: "1",
+        },
+        "100%": {
+          transform: "scale(2)",
+          opacity: "0",
+        },
+      },
+
+      pulse: {
+        "0%, 100%": {
+          opacity: "1",
+        },
+        "50%": {
+          opacity: ".5",
+        },
+      },
+
+      bounce: {
+        "0%, 100%": {
+          transform: "translateY(-10%)",
+        },
+        "50%": {
+          transform: "translateY(0)",
+        },
+      },
+      squeeze: {
+        "0%, 100%": {
+          transform: "scale(1, 1)",
+        },
+        "50%": {
+          transform: "scale(1.1, 0.9)",
+        },
+      },
+    },
   },
   plugins: [],
 };
